91373ad23ca860dd7fec4f1913d44fabf0240e18
vaxsdr <elizabetdaskalova@gmail.com>
2026-06-05 11:22:01 +0000

    Initialize server socket and set SO_REUSEADDR option

--- file-server.c
+++ file-server.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
 
 int main(int argc, char *argv[]) {
-    printf("File Server Skele\n");
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    int port = atoi(argv[1]);   
+    int server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sock < 0) {
+        perror("Socket creation failed");
+        exit(1);
+    }
+
+
+    int opt = 1;
+    setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    printf("Socket created successfully on port %d\n", port);
+    close(server_sock);
     return 0;
 }
